df91ab9cd4ef5107dee8cd6ad62749057b37e9d4
salvatierraggg <salvatierraggg@gmail.com>
2020-02-20 20:49:56 -0300

    tengo que convertir uuna lista de url en una de imagenes sin descargarlas

--- archivos.py
+++ archivos.py
@@ -3,6 +3,8 @@ import os
 # import numpy as np
 from PIL import Image
 from skimage import io
+import requests
+import json
 
 # carpeta_imagenes = './imagenes'
 carpeta_imagenes = '/home/sebas/UNaHur/progConcu_2020verano/python-manipulacion-imagenes/imagenes'
@@ -15,9 +17,25 @@ def leer_imagen(nombre):
 
 def escribir_imagen(nombre, imagen):
   Image.fromarray(imagen).save(armar_ruta(nombre))
+  #esto lo guarda
 
 def leer_imagen2(nombre):
   return io.imread(armar_ruta(nombre))
 
 def escribir_imagen2(nombre, imagen):
   io.imsave(armar_ruta(nombre),imagen)
+
+
+######################copiado  de la clase anterior#############################################
+class Pixabay():
+  def __init__(self, key, carpeta_imagenes):
+    self.key = key
+    self.carpeta_imagenes = carpeta_imagenes
+    
+
+  def buscar_imagenes(self, query, cantidad):
+    #utiliso un url de pixabay
+    url = f'https://pixabay.com/api/?key={self.key}&per_page={cantidad}&q={query}&image_type=photo&lang=es'
+    response = requests.get(url)
+    jsonResponse = json.loads(response.text)
+    return map(lambda h: h['largeImageURL'], jsonResponse['hits'])
--- concatenacion.py
+++ concatenacion.py
@@ -1,18 +1,36 @@
 import numpy as np
 from PIL import Image
-from archivos import leer_imagen, escribir_imagen
+from archivos import *
 
 
-def concatenar_horizontal(imagenes):
+
+
+def concatenar_horizontal(imagenes):#imagenes es una lista de imagenes
   min_img_shape = sorted([(np.sum(i.size), i.size) for i in imagenes])[0][1]
+  
   return np.hstack(list((np.asarray(i.resize(min_img_shape, Image.ANTIALIAS)) for i in imagenes)))
 
 def concatenar_vertical(imagenes):
   min_img_shape = sorted([(np.sum(i.size), i.size) for i in imagenes])[0][1]
   return np.vstack(list((np.asarray(i.resize(min_img_shape, Image.ANTIALIAS)) for i in imagenes)))
 
+
+
+
+
+##########################
+privado=Pixabay('15336424-3010f778fbb10add8cf653c86', "./imagenes")#mi clave y donde guardar
+urls=privado.buscar_imagenes("LOL", 20)
+##########################
+
+
+
+imagenes=[]
+for u in urls:
+  imagenes.append(leer_imagen(u))
+
 imagen1 = leer_imagen('1.jpg')
 imagen2 = leer_imagen('2.jpg')
 
-escribir_imagen('concatenada-vertical.jpg', concatenar_vertical([imagen1, imagen2]))    
-escribir_imagen('concatenada-horizontal.jpg', concatenar_horizontal([imagen1, imagen2]))    
+escribir_imagen('concatenada-vertical.jpg', concatenar_vertical(imagenes))    
+escribir_imagen('concatenada-horizontal.jpg', concatenar_horizontal(imagenes))    
